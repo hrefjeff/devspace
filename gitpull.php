04569bfadd37ba22b8b1c59bd5a67e926ebcbd37
Jeffrey Allen <jeffreyallen92@gmail.com>
2017-05-30 09:45:41 -0500

    Added gitpull script

---
+++ gitpull.php
@@ -0,0 +1,9 @@
+<?php
+
+// This file is used to auto-update the project on a live server when a git-push is initiated
+// Resource used:
+//     https://code.tutsplus.com/tutorials/the-perfect-workflow-with-git-github-and-ssh--net-19564
+
+`git pull`;
+
+?>
